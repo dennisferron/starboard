9cc56c90539b93788e8ebdb8da6a98e946d40f34
Dennis Ferron <system.windows.codeslinger@gmail.com>
2024-07-14 13:45:49 -0500

    Pasted in example usage code, however the example relies on a function not present in latest ANTLR4 cpp runtime, and I don't know what replaced it.

--- main.cpp
+++ main.cpp
@@ -1,7 +1,30 @@
 #include <iostream>
+#include <fstream>
+#include <string>
+#include "antlr4-runtime.h"
+#include "LangLexer.h"
+#include "LangParser.h"
 
-int main(int argc, char* argv[])
-{
-	std::cout << "Hello, world!\n";
-    return 0;
+using namespace antlr4;
+using namespace std;
+
+int main(int argc, char *argv[]) {
+    string line;
+    ifstream LangFile ("examples//example1.txt");
+    if (LangFile.is_open()) {
+        ANTLRInputStream input(LangFile);
+        Lang::LangLexer lexer(&input);
+        CommonTokenStream tokens(&lexer);
+
+        tokens.fill();
+        for (auto token : tokens.getTokens()) {
+            std::cout << token->toString() << std::endl;
+        }
+
+        Lang::LangParser parser(&tokens);
+        tree::ParseTree *tree = parser.stored_definition();
+
+        std::cout << tree->toStringTree(&parser) << std::endl;
+        LangFile.close();
+    }
 }
